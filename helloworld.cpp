f9ba441826a837959c76ab5406bf9117bad5c251
Syrus Muhammed <syrus.muham@gmail.com>
2025-08-03 19:40:24 -0600

    added comments to helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include<iostream> // preprocessor directive, lets us use stuff from the iostream lib, such as std :: cout, would result in errors without it
 
 int main() {
-    std :: cout <<"Hello World";
-    return 0;
+    std :: cout <<"Hello World!";   // "std :: cout" == "character output" | "<<" == displays info to the console
+    return 0;                       // return something to the OS, 0 == "everything went okay!"
 }
